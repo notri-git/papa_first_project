81f16d9c4b70ed1aa0498388e4ead4a5f45e99a1
notri-git <0ks4n4y0d@gmail.com>
2026-07-31 12:44:53 +0500

    задание 2 сделано

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("Привет, мир!")
+print("ДОБРО ПОЖАЛОВАТЬ!")
+print("Команды: /help, /crypt, /decrypt, /exit")
+
+while True:
+    vvod = input()
+
+    if vvod == "":
+        continue
+
+    if vvod == "/help":
+        print("/crypt текст - зашифровать")
+        print("/decrypt текст - расшифровать")
+        print("/exit - выход")
+
+    elif vvod == "/exit":
+        print("Пока!")
+        break
+
+    else:
+        komanda = ""
+        tekst = ""
+        probel_nayden = False
+
+        for bukva in vvod:
+            if bukva == " " and not probel_nayden:
+                probel_nayden = True
+            elif not probel_nayden:
+                komanda = komanda + bukva
+            else:
+                tekst = tekst + bukva
+
+        komanda = komanda.lower()
+
+        if komanda == "/crypt":
+            rezultat = ""
+            for bukva in tekst:
+                rezultat = rezultat + chr(ord(bukva) + 3)
+            print(rezultat)
+
+        elif komanda == "/decrypt":
+            rezultat = ""
+            for bukva in tekst:
+                rezultat = rezultat + chr(ord(bukva) - 3)
+            print(rezultat)
+
+        else:
+            print("Команда не найдена. Введите /help")
