a9467a51a1092b606c9ee68b3f973f6bbf7bcf96
notri-git <0ks4n4y0d@gmail.com>
2026-08-07 22:53:49 +0500

    Задание 4 выполнено

--- config.py
+++ config.py
@@ -1,1 +1,24 @@
-API_KEY = "91cb65ec25500262c0a9185100af3374"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+COMMANDS = [
+    "/help - список команд",
+    "/crypt текст - зашифровать",
+    "/decrypt текст - расшифровать",
+    "/os - информация о системе",
+    "/hostname - имя компьютера",
+    "/pwd - текущая папка",
+    "/ls - список файлов",
+    "/dt - дата и время",
+    "/weather город - узнать погоду",
+    "/note add текст - добавить заметку",
+    "/note ID - показать заметку",
+    "/note del ID - удалить заметку",
+    "/notes - показать все заметки",
+    "/search текст - поиск заметок",
+    "/exit - выход"
+]
